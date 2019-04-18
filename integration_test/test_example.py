de01483a4e52eaa352cc5310ae709dabdbae036d
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-04-18 17:41:40 +0530

    travis integration test reset

--- integration_test/test_example.py
+++ integration_test/test_example.py
@@ -1,11 +1,18 @@
-import requests
 import unittest
+from web.serve import app
 
 class FlaskTestCase(unittest.TestCase):
 
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def tearDown(self):
+        pass
+
     def test_endpoint(self):
-        r = requests.get('http://127.0.0.1:5000/')
-        self.assertEqual(r.status_code, 200)
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
