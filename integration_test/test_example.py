a88123dc874bc0d0c32704e120770b404122902a
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2019-04-23 16:01:52 +0530

    spin up server in background

--- integration_test/test_example.py
+++ integration_test/test_example.py
@@ -3,16 +3,9 @@ from web.serve import app
 
 class FlaskTestCase(unittest.TestCase):
 
-    def setUp(self):
-        app.config['TESTING'] = True
-        self.app = app.test_client()
-
-    def tearDown(self):
-        pass
-
     def test_endpoint(self):
-        response = self.app.get('/', follow_redirects=True)
-        self.assertEqual(response.status_code, 200)
+        r = requests.get('http://127.0.0.1:5000/')
+        self.assertEqual(r.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
